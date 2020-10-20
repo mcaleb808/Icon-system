d12ed5cd4081baeea128d6ce2567df82d0d38256
Mugisha <mcaleb808@gmail.com>
2020-10-20 19:50:31 +0200

    build icon component
    - create dummy data for icon
    - display icon using dummy data

---
+++ src/components/Icon.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import iconPath from '../dummyData';
+
+const defaultStyles = { display: 'inline-block', verticalAlign: 'middle' };
+
+const Icon = ({ size, color, icon, className, style, viewBox }) => {
+  const styles = { ...defaultStyles, ...style };
+  return (
+    <svg
+      className={className}
+      style={styles}
+      viewBox={viewBox}
+      width={`${size}px`}
+      height={`${size}px`}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink">
+      <path fill={color} d={iconPath[icon]} />
+    </svg>
+  );
+};
+
+Icon.defaultProps = {
+  size: 24,
+  color: '#000000',
+  viewBox: '0 0 24 24',
+  style: {},
+  className: '',
+};
+
+Icon.propTypes = {
+  size: PropTypes.number.isRequired,
+  color: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  viewBox: PropTypes.string.isRequired,
+  style: PropTypes.any,
+  className: PropTypes.string,
+};
+
+export default Icon;
